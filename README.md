935d84361671016449d868b85377366918d08f7f
Sunil Kumar <sunilssaharan@gmail.com>
2018-10-22 00:56:56 +0530

    making sounds succesful

---
+++ README.md
@@ -0,0 +1,3 @@
+
+# Run this command if face any error
+## sudo apt-get install festival festvox-kallpc16k
---
+++ app.js
@@ -0,0 +1,15 @@
+//var TextToSpeech = require("./node_modules/text-to-speech-js/lib/index");
+//var TextToSpeech = require("text-to-speech-js");
+
+var say = require('say');
+
+say.speak('Hello, how are you today?', (err) => {
+    if (err) {
+        return console.error(err);
+    }
+
+    console.log('Text has been spoken.');
+});
+
+
+//TextToSpeech.talk("Hello Beautiful World!");
